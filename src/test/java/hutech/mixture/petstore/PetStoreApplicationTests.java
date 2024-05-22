b551a662ee1719f642cef54930424c105287d369
Hoàng <vanhoang1231234@gmail.com>
2024-05-23 01:05:32 +0700

    init project

---
+++ src/test/java/hutech/mixture/petstore/PetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hutech.mixture.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
