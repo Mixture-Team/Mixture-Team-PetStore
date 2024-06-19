d3a7976a715371d31f2dea372a96e1704aa81c50
Hoàng <vanhoang1231234@gmail.com>
2024-06-19 15:36:57 +0700

    feat: test

---
+++ src/main/java/hutech/mixture/petstore/repositories/IRoleRepository.java
@@ -0,0 +1,8 @@
+package hutech.mixture.petstore.repositories;
+
+import hutech.mixture.petstore.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRoleRepository extends JpaRepository<Role, Long> {
+    Role findRoleById(Long id);
+}
