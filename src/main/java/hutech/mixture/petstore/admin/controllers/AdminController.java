c396bd348ad04cf6d48b88da0c56807afda832a4
Hoàng <vanhoang1231234@gmail.com>
2024-07-01 13:08:54 +0700

    refactor: directory

--- src/main/java/hutech/mixture/petstore/admin/controllers/AdminController.java
+++ src/main/java/hutech/mixture/petstore/admin/controllers/AdminController.java
@@ -1,4 +1,4 @@
-package hutech.mixture.petstore.controllers.admin;
+package hutech.mixture.petstore.admin.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
